52cf1869a9dafd9b2d3b956d88e44bf73e4c47ac
Student <student@Makerss-MacBook-Air-6.local>
2020-03-03 16:27:56 +0000

    Add an index.html page for the app - incomplete

--- note-list-view-test.js
+++ note-list-view-test.js
@@ -1,4 +1,19 @@
 (function(exports){
+    function testNoteListViewWhenEmpty() {
+        // var noteList = new NoteList();
+        // var noteListView = new NoteListView(noteList);
+        if (noteListView.output() !== "<ul></ul>") {
+            throw new Error("NoteListViewtest when empty failure")
+        }
+        else {
+            console.log("testNoteListView when empty passes")
+        }
+    } 
+    testNoteListViewWhenEmpty();
+})(this);
+    
+    
+    (function(exports){
     function testNoteListView() {
         var noteList = new NoteList();
         noteList.newNote("Hi Liz!")
